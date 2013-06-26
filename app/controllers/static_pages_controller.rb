0d06d976bd903c2ccbc2be2b484756e5f9a7ee44
Victor Carreon <djlimbs@Victors-MacBook-Pro.local>
2013-06-26 10:34:35 -0500

    updated sign in with dynamic forms based on usertype

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,14 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+	
+  	def home
+  	end
 
-  def djs
-  end
+  	def djs
+  	end
 
-  def guests
-  end
+  	def guests
+  	end
 
-  def couples
-  end
+  	def couples
+  	end
 end
