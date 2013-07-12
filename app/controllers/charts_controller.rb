6d5282a76a95dafb7a683a98cd4f6c958968e1b3
Victor Carreon <djlimbs@Victors-MacBook-Pro.local>
2013-07-12 09:14:40 -0500

    add missing files

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,34 @@
+class ChartsController < ApplicationController
+  before_action :check_admin
+  before_action :set_chart, only: [:show, :edit, :destroy, :update]
+
+  def index
+  	@charts = Chart.all
+  end
+ 
+  def new
+  end
+  
+  def show
+  	@display = @chart.gather_votes
+  end
+
+  def edit
+  end
+  
+  def destroy
+    @chart.destroy
+    respond_to do |format|
+      format.html { redirect_to charts_url }
+      format.json { head :no_content }
+    end
+  end
+  
+  def update
+  end
+  
+  private
+  	def set_chart
+  		@chart = Chart.find(params[:id])
+  	end
+end
